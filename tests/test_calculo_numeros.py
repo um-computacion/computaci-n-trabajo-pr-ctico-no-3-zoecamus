fec90499aa18fdc31c761dbe394526d4c8969e17
Zoe <z.camus@alumno.um.edu.ar>
2025-04-12 11:59:37 -0300

    primer issue

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -1,35 +1,12 @@
 import unittest
-from src.exceptions import (
-    ingrese_numero,
-    NumeroDebeSerPositivo,
-)
 from unittest.mock import patch
 
 class TestCalculoNumeros(unittest.TestCase):
 
-    @patch(  # este patch controla lo que hace el input
-        'builtins.input',
-        return_value='100'
-    )
-    def test_ingreso_feliz(self, patch_input):
+    @patch( 'builtins.input', return_value='10')
+    def test_ingreso_positivo(self, patch_input):
         numero = ingrese_numero()
-        self.assertEqual(numero, 100)
-
-    @patch(  # este patch controla lo que hace el input
-        'builtins.input',
-        return_value='-100'
-    )
-    def test_ingreso_negativo(self, patch_input):
-        with self.assertRaises(NumeroDebeSerPositivo):
-            ingrese_numero()
-
-    @patch(  # este patch controla lo que hace el input
-        'builtins.input',
-        return_value='AAA'
-    )
-    def test_ingreso_letras(self, patch_input):
-        with self.assertRaises(ValueError):
-            ingrese_numero()
+        self.assertEqual(numero, 10)
 
 if __name__ == '__main__':
     unittest.main() 
