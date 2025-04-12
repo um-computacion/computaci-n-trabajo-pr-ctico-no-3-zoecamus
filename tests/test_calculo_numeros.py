ebffe3449f783f6479d0828fbd3e0d8ab1ca11bb
Zoe <z.camus@alumno.um.edu.ar>
2025-04-12 12:19:11 -0300

    segundo issue, test funcion negativa

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -8,5 +8,11 @@ class TestCalculoNumeros(unittest.TestCase):
         numero = ingrese_numero()
         self.assertEqual(numero, 10)
 
+
+    @patch( 'builtins.input', return_value='-10')
+    def test_ingreso_negativo(self, patch_input):
+        numero = ingrese_numero()
+        self.assertEqual(numero, -10)
+
 if __name__ == '__main__':
     unittest.main() 
